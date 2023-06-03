d4e3aee2af2d3a7f813ca119b7b25278dc8db3f5
NaniXC7 <dcabreraa2@miumg.edu.gt>
2023-06-03 14:02:56 -0600

    Se subió totito al repositorio

---
+++ src/main/java/com/mycompany/totito/Totito.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.totito;
+
+/**
+ *
+ * @author NaniXC7
+ */
+public class Totito {
+
+    public static void main(String[] args) {
+        Juego ventana = new Juego();
+        ventana.setVisible(true);
+    }
+}
